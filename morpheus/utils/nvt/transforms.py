8f929db8c39ef103aebe118a75905d45d56253b8
David Gardner <dagardner@nvidia.com>
2023-07-10 14:41:34 -0700

    Capture the value of 'col' when definign the lambda, avoids pylint's cell-var-from-loop warning

--- morpheus/utils/nvt/transforms.py
+++ morpheus/utils/nvt/transforms.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import json
 import typing
 
@@ -32,7 +31,7 @@ def json_flatten(col_selector, df: typing.Union[pd.DataFrame, cudf.DataFrame]):
         pd_series = df[col] if not convert_to_cudf else df[col].to_pandas()
         pd_series = pd_series.apply(lambda x: x if isinstance(x, dict) else json.loads(x))
         pdf_norm = pd.json_normalize(pd_series)
-        pdf_norm.rename(columns=lambda x: col + "." + x, inplace=True)
+        pdf_norm.rename(columns=lambda x, c=col: c + "." + x, inplace=True)
         pdf_norm.reset_index(drop=True, inplace=True)
 
         if (df_normalized is None):
